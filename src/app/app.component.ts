b04f81ef0f3d5e055ee9b9b7aea6785113a5da89
Jack Torres <jack.torres@matrixcare.com>
2017-10-22 21:06:07 -0400

    182 - Updating objects in firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
 import { Component, OnDestroy } from '@angular/core';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +9,40 @@ import { AngularFireDatabase } from 'angularfire2/database';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  courses$;
+  coursesRef: AngularFireList<any>;
+  courses$: Observable<any>;
   course$;
   author$;
 
-  constructor(db: AngularFireDatabase) {
-    this.courses$ = db.list('/courses').valueChanges();
+  constructor(private db: AngularFireDatabase) {
+    this.coursesRef = db.list('/courses');
+    this.courses$ = this.coursesRef.snapshotChanges().map(changes => {
+      return changes.map(c => ({ 
+        key: c.payload.key, 
+        val: c.payload.val() 
+      }));
+    });
     this.course$ = db.object('/courses/1').valueChanges();
     this.author$ = db.object('/authors/1').valueChanges();
+  }
+
+  add(course: HTMLInputElement){
+    this.coursesRef.push({
+      name: course.value,
+      price: 150,
+      isLive: true,
+      sections: [
+        { title: 'Components' },
+        { title: 'Directives' },
+        { title: 'Templates' },
+      ]
+    });
+  }
+
+  update(course) {
+    this.coursesRef.update(course.key, {
+      title: course.val + ' Updated',
+      price: 160
+    });
   }
 }
